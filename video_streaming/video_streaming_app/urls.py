e74f01e588308352ad51c033a16bc64f55e47c2b
sohammukher <sohammukherjee1996@gmail.com>
2023-04-02 23:10:20 -0400

    created basic templates

--- video_streaming/video_streaming_app/urls.py
+++ video_streaming/video_streaming_app/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('video_streaming_app.url'))
+    path('', views.index, name='index'),
 ]
